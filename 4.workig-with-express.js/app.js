a59fe5f4fd9720c3bcce4e5fd423e2b0cc6adfba
Yash Bansal <Yash.Bansal@wissen.com>
2024-05-13 11:06:25 +0530

    Add nodemon and started working with express.js

---
+++ 4.workig-with-express.js/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const http = require('http');
+//const fs = require('fs');
+
+
+const server = http.createServer(
+
+);
+
+server.listen(3000); //tells the server to listen on a particular port, we can pass a port of hostname as argument.
